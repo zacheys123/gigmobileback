55765db1c33a6276e52c90618fc6fba7d6c476e1
zacharia muigai <zacheyshop@gmail.com>
2024-12-03 03:48:30 +0300

    new back

--- server.js
+++ server.js
@@ -15,10 +15,18 @@ dotenv.config(); // Load environment variables
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
 
 connectDB();
+app.use(express.json({ limit: "100mb", extended: true }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use("/auth", authRoutes);
 app.use("/posts", postsRoutes);
 app.use("/users", usersRoutes);
