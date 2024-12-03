67def5242a043ed043b1d515348f292e8c0eaba6
zacharia muigai <zacheyshop@gmail.com>
2024-12-03 17:54:56 +0300

    new back

--- controllers/posts/getposts.js
+++ controllers/posts/getposts.js
@@ -1,4 +1,6 @@
-import { posts } from "../../utils.js";
+import Post from "../../models/post.js";
+
 export const getposts = async (req, res, next) => {
+  const posts = await Post.find();
   return res.status(200).json({ success: true, result: posts });
 };
--- models/post.js
+++ models/post.js
@@ -22,5 +22,4 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 const Post = mongoose.model("Post", postSchema);
-
 export default Post;
